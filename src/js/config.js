68770eb44c455fa97f433a9a2be486c664f966b3
Kell-Stack <KellySousa1920@gmail.com>
2019-09-17 15:06:08 -0700

    merge with working UI-component-disappear-after-module-split branch

--- src/js/config.js
+++ src/js/config.js
@@ -2,22 +2,46 @@ import $ from 'jquery';
 import * as kintoneJSSDK from '@kintone/kintone-js-sdk';
 var kintoneUIComponent = require('modules/@kintone/kintone-ui-component/dist/kintone-ui-component.min.js');
 require('modules/@kintone/kintone-ui-component/dist/kintone-ui-component.min.css');
-import {
-  setTable
-} from './configTable'
+import {setTable} from './configTable'
 
 
-(function (PLUGIN_ID) {
+(function(PLUGIN_ID) {
   'use strict';
 
   var connection = new kintoneJSSDK.Connection()
   var kintoneApp = new kintoneJSSDK.App(connection)
 
   kintoneApp.getFormLayout(kintone.app.getId(), true).then((rsp) => {
-
-
+  
+  
     var tableRecords = event.record.order_items.value;
-    console.log("👹", tableRecords)
+  console.log("👹",tableRecords)
+
+}).catch((err) => {
+  console.log(err);
+});
+
+    var initialData = [{
+        column: {
+          items: [
+               {
+                   label: 'Cars',
+                   value: 'cars',
+                   isDisabled: false
+               },
+               {
+                   label: 'Robots',
+                   value: 'robots',
+                   isDisabled: false
+               },
+               {
+                   label: 'Animals',
+                   value: 'animals',
+                   isDisabled: true
+               },
+           ],
+          value: 'cars'
+        },
 
         filter: {
           items: [
@@ -39,34 +63,15 @@ import {
            ],
           value: 'cars'
         },
-        {
-          label: 'Animals',
-          value: 'animals',
-          isDisabled: true
-        },
-      ],
-      value: 'cars'
-    },
-    filter: {
-      items: [{
-          label: 'Cars',
-          value: 'cars',
-          isDisabled: false
-        },
-        {
-          header: 'Filter',
-          cell: function() { return kintoneUIComponent.createTableCell('dropdown', 'filter') }
-        },
-      ],
-      value: 'cars'
-    },
+        
+      },
+    ];
+  
+    var table = setTable(initialData)
 
-  }, ];
-
-  var table = setTable(initialData)
-
-  $('.settings').append(table.render());
+    $('.settings').append(table.render());
 
 
 
-})(kintone.$PLUGIN_ID);
+  })(kintone.$PLUGIN_ID);
+
